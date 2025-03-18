41e75f23548f0b40a47b99c4cad909587c9419bb
omsharma29 <theomsharma100@gmail.com>
2025-03-18 23:43:11 +0530

    Logic of address fetch

--- apps/webpage/src/lib/firebase-auth.ts
+++ apps/webpage/src/lib/firebase-auth.ts
@@ -2,6 +2,8 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { GoogleAuthProvider } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
@@ -17,4 +19,6 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const provider = new GoogleAuthProvider();
+export const db = getFirestore(app);
+
 
